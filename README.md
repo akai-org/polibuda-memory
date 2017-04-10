38f0f94e3ccad0beabbe17b9e239e5a98ce373e2
Marcin Lawniczak <marcin.safmb@wp.pl>
2017-04-10 22:09:18 +0200

    Update gulp, change folder structure.

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ How well do you know your faculty?
 
 For the overall design and idea, see [DESIGN.md](https://github.com/akai-org/polibuda-memory/blob/master/DESIGN.md)
 
+## Running the project
+
+1. `git clone https://github.com/akai-org/polibuda-memory.git`
+2. `npm install` (requires node.js installed)
+3. `gulp`
+
 ## Acknowledgements
 Marcin Ławniczak [@marcinlawnik](https://github.com/marcinlawnik)
--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,25 @@ var sass        = require('gulp-sass');
 var babel       = require('gulp-babel');
 var concat      = require('gulp-concat');
 var sourcemaps  = require('gulp-sourcemaps');
+var fileinclude = require('gulp-file-include');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'babel'], function() {
+gulp.task('serve', ['html', 'images', 'sass', 'babel', 'json'], function() {
 
   browserSync.init({
     server: "./public"
   });
 
-  gulp.watch("assets/stylesheets/*.scss", ['sass']);
-  gulp.watch("assets/javascripts/*.js", ['babel']);
-  gulp.watch("public/*.html").on('change', browserSync.reload);
+    gulp.watch("app/stylesheets/*.scss", ['sass']);
+    gulp.watch("app/javascripts/**/*.js", ['babel']);
+    gulp.watch("app/json/*.json", ['json']);
+    gulp.watch("app/*.html", ['html']);
+    gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src("assets/stylesheets/*.scss")
+  return gulp.src("app/stylesheets/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write('.'))
@@ -28,7 +31,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('babel', function() {
-  return gulp.src("assets/javascripts/*.js")
+  return gulp.src("app/javascripts/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
@@ -36,6 +39,25 @@ gulp.task('babel', function() {
     .pipe(concat("all.js"))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest("public/js"));
+});
+
+gulp.task('html', function() {
+    return gulp.src("app/*.html")
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest("public/"));
+});
+
+gulp.task('json', function() {
+    return gulp.src("app/json/*.json")
+        .pipe(gulp.dest("public/json/"));
+});
+
+gulp.task('images', function() {
+    return gulp.src("app/images/**/*")
+        .pipe(gulp.dest("public/images/"));
 });
 
 gulp.task('default', ['serve']);
